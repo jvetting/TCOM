d34c54a6608dc3f550e6f37de8980218c9ee9004
Anthony Ruffalo <antruffii@gmail.com>
2019-09-10 19:14:31 -0500

    Edited Main and HexTile to create multiple tiles.

--- Experiments/core/src/com/mygdx/game/HexTile.java
+++ Experiments/core/src/com/mygdx/game/HexTile.java
@@ -7,35 +7,30 @@ public class HexTile extends ApplicationAdapter {
     ShapeRenderer shapeRenderer;
     private float[] vertices;
 
-    HexTile() {
-        create();
-    }
-
-    @Override
-    public void create() {
+    HexTile(float xOff, float yOff) {
         shapeRenderer = new ShapeRenderer();
         vertices = new float[12];
-        tempSetVertices();
+        tempSetVertices(xOff, yOff);
     }
 
-    private void tempSetVertices(){
-        vertices[0] = 225f;
-        vertices[1] = 200f;
+    private void tempSetVertices(float xOff, float yOff){
+        vertices[0] = 25f + xOff;
+        vertices[1] = 0f + yOff;
 
-        vertices[2] = 275f;
-        vertices[3] = 200f;
+        vertices[2] = 75f + xOff;
+        vertices[3] = 0f + yOff;
 
-        vertices[4] = 300f;
-        vertices[5] = 250f;
+        vertices[4] = 100f + xOff;
+        vertices[5] = 50f + yOff;
 
-        vertices[6] = 275f;
-        vertices[7] = 300f;
+        vertices[6] = 75f + xOff;
+        vertices[7] = 100f + yOff;
 
-        vertices[8] = 225f;
-        vertices[9] = 300f;
+        vertices[8] = 25f + xOff;
+        vertices[9] = 100f + yOff;
 
-        vertices[10] = 200f;
-        vertices[11] = 250f;
+        vertices[10] = 0f + xOff;
+        vertices[11] = 50f + yOff;
     }
 
     public void drawHex(){
@@ -43,11 +38,6 @@ public class HexTile extends ApplicationAdapter {
         shapeRenderer.setColor(1,1,1,1);
         shapeRenderer.polygon(vertices);
         shapeRenderer.end();
-    }
-
-    @Override
-    public void render() {
-        drawHex();
     }
 
     @Override
--- Experiments/core/src/com/mygdx/game/Main.java
+++ Experiments/core/src/com/mygdx/game/Main.java
@@ -11,14 +11,50 @@ public class Main extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	BitmapFont font;
-	HexTile hex;
+	float[] xOff;
+	float[] yOff;
+	HexTile[] hex;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 		font = new BitmapFont();
-		hex = new HexTile();
+		xOff = new float[12];
+		yOff = new float[12];
+		setOffsets();
+		hex = new HexTile[12];
+		int i;
+		for (i = 0; i < hex.length; i++){
+			hex[i] = new HexTile(xOff[i], yOff[i]);
+		}
+	}
+
+	private void setOffsets(){
+		xOff[0] = 0f;
+		yOff[0] = 0f;
+		xOff[1] = 100f;
+		yOff[1] = 0f;
+		xOff[2] = 200f;
+		yOff[2] = 0f;
+		xOff[3] = 300f;
+		yOff[3] = 0f;
+		xOff[4] = 400f;
+		yOff[4] = 0f;
+		xOff[5] = 0f;
+		yOff[5] = 100f;
+		xOff[6] = 100f;
+		yOff[6] = 100f;
+		xOff[7] = 200f;
+		yOff[7] = 100f;
+		xOff[8] = 300f;
+		yOff[8] = 100f;
+		xOff[9] = 400f;
+		yOff[9] = 100f;
+		xOff[10] = 0f;
+		yOff[10] = 200f;
+		xOff[11] = 100f;
+		yOff[11] = 200f;
 	}
 
 	@Override
@@ -26,10 +62,12 @@ public class Main extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		font.draw(batch, "Happy Coding", Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
-		batch.draw(img, 0, 0);
+		//font.draw(batch, "Happy Coding", Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
+		//batch.draw(img, 0, 0);
 		batch.end();
-		hex.drawHex();
+		for (int i = 0; i < hex.length; i++) {
+			hex[i].drawHex();
+		}
 	}
 	
 	@Override
@@ -37,6 +75,10 @@ public class Main extends ApplicationAdapter {
 		batch.dispose();
 		font.dispose();
 		img.dispose();
-		hex.dispose();
+		for (int i = 0; i < hex.length; i++){
+			hex[i].dispose();
+		}
+		xOff = null;
+		yOff = null;
 	}
 }
