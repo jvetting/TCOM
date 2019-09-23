6eca11975bd2b963e4facd0182b5bce52b006cbb
Jonathan Vetting <jvetting@iastate.edu>
2019-09-22 21:52:07 -0500

    Added tables page

--- BackEnd/tcom/target/classes/db/mysql/data.sql
+++ BackEnd/tcom/target/classes/db/mysql/data.sql
@@ -1,3 +1,3 @@
-INSERT IGNORE INTO tiles VALUES (NULL, 'Obamas Last Name');
-INSERT IGNORE INTO tiles VALUES (NULL, 'Ben Shapiro');
-INSERT IGNORE INTO tiles VALUES (NULL, 'Andrew Anubis');
+INSERT IGNORE INTO tiles VALUES (1, 'Obamas Last Name');
+INSERT IGNORE INTO tiles VALUES (2, 'Ben Shapiro');
+INSERT IGNORE INTO tiles VALUES (69, 'Andrew Anubis');
